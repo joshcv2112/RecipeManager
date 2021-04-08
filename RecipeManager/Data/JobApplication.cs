2ded438aed7b37f8d778d7fa537c1008e9bfb143
joshcv2112 <joshcv2112@gmail.com>
2021-04-08 12:51:27 -0700

    forms example

---
+++ RecipeManager/Data/JobApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// TEMPORARY FILE JUST FOR EXAMPLE, DELETE LATER
+
+namespace RecipeManager.Data
+{
+    public class JobApplication
+    {
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public int SalaryExpectations { get; set; }
+        public bool DoesOpenSource { get; set; }
+        public DateTime Availability { get; set; }
+    }
+}
