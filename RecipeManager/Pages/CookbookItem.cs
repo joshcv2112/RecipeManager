c6c5bd0188a37d875d3074fecf38b82410bd0be1
Joshua Vaughan <joshcv2112@gmail.com>
2021-04-08 22:25:04 -0700

    Renamed Cookbook->CookbookItem.cs

---
+++ RecipeManager/Pages/CookbookItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Pages
+{
+    public class CookbookItem
+    {
+        [Required]
+        public string CookbookTitle { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "Description exceeded max length")]
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
