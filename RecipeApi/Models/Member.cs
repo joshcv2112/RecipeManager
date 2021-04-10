afb1c65849affe1ba0b50bce3495b149df859917
Joshua Vaughan <joshcv2112@gmail.com>
2021-04-10 11:04:05 -0700

    Created sample API with EF migration

---
+++ RecipeApi/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeApi.Models
+{
+    public class member
+    {
+        [Key]
+        public long Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string MiddleName { get; set; }
+    }
+}
