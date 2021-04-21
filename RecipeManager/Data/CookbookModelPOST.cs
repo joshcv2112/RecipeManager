ccef487f3c2b2be027af838e20c156771880e556
Joshua Vaughan <joshcv2112@gmail.com>
2021-04-20 22:29:22 -0700

    Hooked up add recipe to API

---
+++ RecipeManager/Data/CookbookModelPOST.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace RecipeManager.Data
+{
+    public class CookbookModelPOST
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+    }
+}
