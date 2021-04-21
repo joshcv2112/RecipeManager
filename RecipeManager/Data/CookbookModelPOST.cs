25d65ea57e4039ed858ec731acb7bd93ddc46138
joshcv2112 <joshcv2112@gmail.com>
2021-04-21 15:43:39 -0700

    fixed misc UI bugs

--- RecipeManager/Data/CookbookModelPOST.cs
+++ RecipeManager/Data/CookbookModelPOST.cs
@@ -11,5 +11,8 @@ namespace RecipeManager.Data
         public string Description { get; set; }
         [JsonProperty("imageUrl")]
         public string ImageUrl { get; set; }
+        [JsonProperty("userId")]
+        // For the moment I am hardcoding this value, later I will make the UI send the Id of the currently logged in user.
+        public Guid userId = new Guid("98a0866a-a36e-4038-8f68-6b052418e2fa");
     }
 }
