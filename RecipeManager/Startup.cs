1bfb642bf052b8d9cb070dc13d8314f43ba7a64a
Joshua Vaughan <joshcv2112@gmail.com>
2021-05-25 14:05:30 -0700

    Implement KeyVault for UI connection string

--- RecipeManager/Startup.cs
+++ RecipeManager/Startup.cs
@@ -27,8 +27,7 @@ namespace RecipeManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetSection("RDB-Connection").Value));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
